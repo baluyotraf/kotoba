cbdf3702661337cc1f8d8ed46c69c81a59cafd6c
Raffaello Baluyot <raf.baluyot@ivantageapps.com>
2018-09-07 09:16:31 +0800

    Added initial preprocessing modules

--- kotoba/__init__.py
+++ kotoba/__init__.py
@@ -1,1 +1,36 @@
+from abc import ABCMeta, abstractmethod
+from ._utils import apply_to_nested
 from . import embedding
+
+
+class Preprocessor(metaclass=ABCMeta):
+
+    @abstractmethod
+    def transform(self, x):
+        pass
+
+
+class Pipeline(Preprocessor):
+
+    def __init__(self, preprocessors):
+        self._preprocessors = preprocessors
+
+    def transform(self, x):
+        for p in self._preprocessors:
+            x = p(x)
+        return x
+
+
+class FunctionPreprocessor(Preprocessor):
+
+    def __init__(self, func):
+        self._func = func
+
+    def transform(self, x):
+        return apply_to_nested(x, self._func)
+
+
+class LowerCase(FunctionPreprocessor):
+
+    def __init__(self):
+        super().__init__(lambda x: x.lower())
