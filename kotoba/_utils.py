07c0b7830f811e652a5dd1972d2aaff62a2b9edd
Raffaello Baluyot <raf.baluyot@ivantageapps.com>
2018-09-07 09:07:08 +0800

    Moved utility functions in embedding to _utils

---
+++ kotoba/_utils.py
@@ -0,0 +1,20 @@
+from itertools import filterfalse
+
+
+def apply_to_nested(data, func=lambda x: x):
+    try:
+        iter(data)
+        if isinstance(data, str):
+            return func(data)
+        else:
+            return [_apply_to_nested(i, func) for i in data]
+    except TypeError:
+        return func(data)
+
+
+def uniquify(iterable):
+    seen = set()
+    seen_add = seen.add
+    for element in filterfalse(seen.__contains__, iterable):
+        seen_add(element)
+        yield element
