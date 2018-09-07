f87d8130fd72c7dc40916251794d649860547be8
Raffaello Baluyot <raf.baluyot@ivantageapps.com>
2018-09-07 14:37:28 +0800

    Moved modules in __init__ to preprocess

---
+++ kotoba/preprocess.py
@@ -0,0 +1,35 @@
+from abc import ABCMeta, abstractmethod
+from ._utils import map_elements
+
+
+class Preprocessor(metaclass=ABCMeta):
+
+    @abstractmethod
+    def transform(self, x):
+        pass
+
+
+class Pipeline(Preprocessor):
+
+    def __init__(self, preprocessors):
+        self._preprocessors = preprocessors
+
+    def transform(self, x):
+        for p in self._preprocessors:
+            x = p.transform(x)
+        return x
+
+
+class FunctionPreprocessor(Preprocessor):
+
+    def __init__(self, func):
+        self._func = func
+
+    def transform(self, x):
+        return map_elements(x, self._func)
+
+
+class LowerCase(FunctionPreprocessor):
+
+    def __init__(self):
+        super().__init__(lambda x: x.lower())
