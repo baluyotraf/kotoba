ebe67bb8b7351ddd201d76cbc60ba53c8c5d3660
Raffaello Baluyot <raf.baluyot@ivantageapps.com>
2018-09-07 15:34:48 +0800

    Moved the iterable option to the transform function

--- kotoba/preprocess.py
+++ kotoba/preprocess.py
@@ -5,32 +5,33 @@ from ._utils import map_elements
 class Preprocessor(metaclass=ABCMeta):
 
     @abstractmethod
-    def transform(self, x):
+    def transform(self, x, as_iterable=False):
         pass
 
 
 class Pipeline(Preprocessor):
 
     def __init__(self, preprocessors):
-        self._preprocessors = preprocessors
+        self._preprocessors = preprocessors[:-1]
+        self._final_preprocessor = preprocessors[-1]
 
-    def transform(self, x):
+    def transform(self, x, as_iterable=False):
         for p in self._preprocessors:
-            x = p.transform(x)
+            x = p.transform(x, as_iterable=True)
+        x = self._final_preprocessor.transform(x, as_iterable)
         return x
 
 
 class FunctionPreprocessor(Preprocessor):
 
-    def __init__(self, func, as_iterable=False):
+    def __init__(self, func):
         self._func = func
-        self._as_iterable = as_iterable
 
-    def transform(self, x):
-        return map_elements(x, self._func, self._as_iterable)
+    def transform(self, x, as_iterable=False):
+        return map_elements(x, self._func, as_iterable)
 
 
 class LowerCase(FunctionPreprocessor):
 
-    def __init__(self, as_iterable=False):
-        super().__init__(lambda x: x.lower(), as_iterable)
+    def __init__(self):
+        super().__init__(lambda x: x.lower())
