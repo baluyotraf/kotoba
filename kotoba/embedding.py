550b5d8e0732103f21f4fa87ea56e5169cf39856
Raffaello Baluyot <raf.baluyot@ivantageapps.com>
2018-09-10 10:51:55 +0800

    TokenEmbedding is now an abstract class BasicEmbedding provide an implementation of the TokenEmbedding

--- kotoba/embedding.py
+++ kotoba/embedding.py
@@ -1,133 +1,105 @@
-import numpy as np
-import os
-import enum
-import json
-from itertools import chain
-from ._utils import map_elements, uniquify
+from abc import ABCMeta, abstractmethod
+from ._utils import uniquify, flatten, create_directory
+from enum import Enum
 
 
-class SpecialTokens(enum.Enum):
+class SpecialTokens(Enum):
     PAD = '<PAD>'
     UNKNOWN = '<UNK>'
 
 
-class TokenEmbedding:
-    def __init__(self, token_list):
-        token_list = uniquify(token_list)
-        index_to_token = list(SpecialTokens)
-        index_to_token.extend(token_list)
-        count = len(index_to_token)
+class TokenEmbedding(metaclass=ABCMeta):
 
-        self._token_to_index = dict(zip(index_to_token, range(count)))
+    @abstractmethod
+    def token_to_id(self, token):
+        pass
+
+    @abstractmethod
+    def id_to_token(self, id_):
+        pass
+
+    @abstractmethod
+    def token_coverage(self, tokens):
+        pass
+
+    @abstractmethod
+    def id_coverage(self, ids):
+        pass
+
+    @abstractmethod
+    def export_token_list(self, path):
+        pass
+
+
+class BasicEmbedding(TokenEmbedding):
+    def __init__(self, token_list, add_pad=False):
+        index_to_token = self._create_index_token_list(token_list, add_pad)
         self._index_to_token = tuple(index_to_token)
+        self._token_to_index = self._create_token_index_dict(index_to_token)
         self._token_set = frozenset(index_to_token)
 
-    def token_to_index(self, tokens):
-        default = self._token_to_index[SpecialTokens.UNKNOWN]
+    # noinspection PyMethodMayBeStatic
+    def _check_special_chars(self, x):
+        try:
+            SpecialTokens(x)
+        except ValueError:
+            return True
+        raise ValueError('{} is a reserved keyword'.format(x))
 
-        def _token_to_index(token):
-            return self._token_to_index.get(token, default)
+    # noinspection PyMethodMayBeStatic
+    def _create_index_token_list(self, token_list, add_pad):
+        token_list = uniquify(flatten(token_list))
+        token_list = filter(self._check_special_chars, token_list)
+        index_to_token = []
+        if add_pad:
+            index_to_token.append(SpecialTokens.PAD.value)
+        index_to_token.extend(token_list)
+        return index_to_token
 
-        return map_elements(tokens, _token_to_index)
+    # noinspection PyMethodMayBeStatic
+    def _create_token_index_dict(self, index_token_list):
+        count = len(index_token_list)
+        return dict(zip(index_token_list, range(count)))
 
-    def index_to_token(self, indices, strict=False):
+    def token_to_id(self, token):
+        default = len(self._token_to_index) + 1
+        return self._token_to_index.get(token, default)
 
-        def _index_to_token(index):
-            if index < 0:
-                if strict:
-                    raise IndexError
-                else:
-                    return SpecialTokens.UNKNOWN
-            else:
-                try:
-                    return self._index_to_token[index]
-                except IndexError as e:
-                    if strict:
-                        raise IndexError from e
-                    else:
-                        return SpecialTokens.UNKNOWN
+    def id_to_token(self, id_):
+        if id_ < 0:
+            return SpecialTokens.UNKNOWN.value
+        else:
+            try:
+                return self._index_to_token[id_]
+            except IndexError:
+                return SpecialTokens.UNKNOWN.value
 
-        return map_elements(indices, _index_to_token)
+    # noinspection PyMethodMayBeStatic
+    def _get_coverage(self, base_set, target_list):
+        target_set = set(flatten(target_list))
+        common_entries = base_set & target_set
+        return len(common_entries) / len(target_set)
 
-    def coverage(self, token_list):
-        token_list = set(token_list)
-        common = self._token_set & token_list
-        return len(common) / len(token_list)
+    def token_coverage(self, tokens):
+        return self._get_coverage(self._token_set, tokens)
 
-    def __contains__(self, item):
-        return item in self._token_set
+    def id_coverage(self, ids):
+        ids_set = set(range(len(self._token_set)))
+        return self._get_coverage(ids_set, ids)
 
-    def save(self, path, encoding='utf-8'):
-        parent_dir = os.path.dirname(path)
-        if parent_dir:
-            os.makedirs(parent_dir, exist_ok=True)
-
-        token_list = self._index_to_token[len(SpecialTokens):]
+    def export_token_list(self, path, encoding='utf-8'):
+        create_directory(path)
         with open(path, 'w', encoding=encoding) as file:
-            json.dump(token_list, file)
+            file.writelines((l + '\n' for l in self._index_to_token))
 
     @property
     def token_size(self):
         return len(self._index_to_token)
 
-    @classmethod
-    def load(cls, path, encoding='utf-8'):
-        with open(path, 'r', encoding=encoding) as file:
-            token_list = json.load(file)
-        return cls(token_list)
-
-
-class GloveEmbedding(TokenEmbedding):
-
-    def __init__(self, word_list, weights):
-        weights = np.asarray(weights)
-
-        if weights.shape[0] != len(word_list):
-            raise ValueError
-
-        self._weights = weights
-        self._dimensions = weights.shape[1]
-        super().__init__(word_list)
-
     def save(self, path, encoding='utf-8'):
-        word_list = self._index_to_token[len(SpecialTokens):]
-        weights = self.weights
-
-        def line_generator():
-            for word, weight in zip(word_list, weights):
-                str_weights = (str(w) for w in weight)
-                line = ' '.join(chain([word], str_weights))
-                yield line
-                yield '\n'
-
-        with open(path, 'w', encoding=encoding) as f:
-            f.writelines(line_generator())
+        return self.export_token_list(path, encoding)
 
     @classmethod
-    def load(cls, path, encoding='utf-8'):
-        word_list = []
-        weights = []
-        emb_size = None
-        with open(path, 'r', encoding=encoding) as f:
-            for line in f:
-                tokens = line.strip().split(' ')
-                word = tokens[0]
-                vector = [float(w) for w in tokens[1:]]
-
-                word_list.append(word)
-                weights.append(vector)
-
-                if emb_size:
-                    assert emb_size == len(vector)
-                else:
-                    emb_size = len(vector)
-
-        return cls(word_list, weights)
-
-    @property
-    def weights(self):
-        return self._weights
-
-    @property
-    def dimensions(self):
-        return self._dimensions
+    def load(cls, path, encoding='utf-8', add_pad=False):
+        with open(path, 'r', encoding=encoding) as file:
+            return cls([l.strip() for l in file], add_pad)
