eca35f0987035ccec35cbbf19c42986a2f542fbd
Raffaello Baluyot <raf.baluyot@ivantageapps.com>
2018-10-23 14:40:49 +0800

    Added docstrings to itertools

--- kotoba/itertools.py
+++ kotoba/itertools.py
@@ -6,6 +6,11 @@ _is_iterable_exceptions = [str, bytes, Mapping]
 
 
 def is_iterable(iterable):
+    """
+    Checks if an object is an iterable
+    :param iterable: object to check
+    :return: True if iterable else False
+    """
     try:
         iter(iterable)
         for t in _is_iterable_exceptions:
@@ -17,10 +22,21 @@ def is_iterable(iterable):
 
 
 def as_iterable(element):
+    """
+    Converts a single element to a generator
+    :param element: element to convert
+    :return: generator with the given element
+    """
     yield element
 
 
 def map_iterable(data, func):
+    """
+    Applies a function to any iterable and to its iterable elements
+    :param data: iterable or object to apply the function
+    :param func: function used for mapping
+    :return:
+    """
     if is_iterable(data):
         return func((map_iterable(d, func) for d in data))
     else:
@@ -35,6 +51,13 @@ def _map_elements(data, func):
 
 
 def map_elements(data, func, as_iterable=False):
+    """
+    Applies a function to the non-iterable items on all iterable in data
+    :param data: iterable or object to apply the function
+    :param func: function used for mapping
+    :param as_iterable: defines if the result must be an iterable or a list
+    :return:
+    """
     mapped = _map_elements(data, func)
     if not as_iterable:
         mapped = map_iterable(mapped, list)
@@ -42,6 +65,11 @@ def map_elements(data, func, as_iterable=False):
 
 
 def flatten(data):
+    """
+    Collapses all iterable items into a single iterable
+    :param data: iterable or object to collapse
+    :return: flattened iterable
+    """
     if is_iterable(data):
         return chain.from_iterable((flatten(d) for d in data))
     else:
@@ -49,6 +77,11 @@ def flatten(data):
 
 
 def uniquify(iterable):
+    """
+    Removes duplicate elements in an iterable while preserving order
+    :param iterable: iterable find the unique elements
+    :return: iterable of the unique elements
+    """
     seen = set()
     seen_add = seen.add
     for element in filterfalse(seen.__contains__, iterable):
@@ -67,6 +100,13 @@ def _batch(iterable, n):
 
 
 def batch(iterable, n, as_iterable=False):
+    """
+    Groups an iterable into an iterable of n elements
+    :param iterable: iterable to batch
+    :param n: number of grouped elements
+    :param as_iterable: defines if the result must be an iterable or a list
+    :return: batched data iterable
+    """
     batched = _batch(iterable, n)
     if not as_iterable:
         batched = map_iterable(batched, list)
@@ -74,6 +114,13 @@ def batch(iterable, n, as_iterable=False):
 
 
 def map_last_dim(iterable, func, as_iterable=False):
+    """
+    Applies the function only to the last dimensions of the iterable
+    :param iterable: iterable to map the last dimension
+    :param func: function for mapping
+    :param as_iterable: defines if the result must be an iterable or a list
+    :return: iterable of the mapped data
+    """
     if is_iterable(iterable):
         iter_list = list(iterable)
         if any((is_iterable(element) for element in iter_list)):
