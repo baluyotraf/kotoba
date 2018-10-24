0f43c190e52e9bfaba9105e59d62d24918981f27
Raffaello Baluyot <raf.baluyot@ivantageapps.com>
2018-10-24 11:54:33 +0800

    Added docstrings to the tokenizer module

--- kotoba/tokenizer.py
+++ kotoba/tokenizer.py
@@ -2,6 +2,11 @@ from .preprocess import MapItems
 
 
 class NLTKTokenizer(MapItems):
+    """
+    A preprocessor using the nltk tokenizer
+    :param language: language used by the tokenizer
+    :param preserve_line: defines is line breaks should be kept as line breaks
+    """
 
     def __init__(self, language='english', preserve_line=False):
         import nltk
